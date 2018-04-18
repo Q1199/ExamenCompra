62d5b19750c48fd53d369c69751cafb5da9c8f79
Administrador <Administrador@172.16.17.107>
2018-04-18 15:03:39 -0600

    Ingresar compras variable prueba

--- src/exaprac2/CernaMiranda.java
+++ src/exaprac2/CernaMiranda.java
@@ -19,7 +19,7 @@ public class CernaMiranda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            
+   int prueba;         
                String productos[]=new String[2]; 
                int cantidad[]=new int[2];
                double precio[]=new double[2];
